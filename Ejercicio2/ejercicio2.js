235ef02f27a7f14999f6902de2d323e6130fa523
draeriel <danieljuanbarbosa@gmail.com>
2019-02-16 23:02:22 +0100

    ejercicio 2 refactor

--- Ejercicio2/ejercicio2.js
+++ Ejercicio2/ejercicio2.js
@@ -1,23 +1,64 @@
 window.onload = function() {
     pList = document.getElementsByTagName('p');
-    document.getElementById('info').onmousemove = function() {
-        pList[0].innerHTML = `Navegador [${event.clientX}, ${event.clientY}]`;   
-        pList[1].innerHTML = `Pàgina [${event.screenX}, ${event.screenY}]`;  
+    document.getElementById('info').onmousemove = function() {onMouseMove()};
+    document.getElementById('info').onmouseup = function() {onMouseUp()};
+    document.getElementById('info').onmousedown = function() {onMouseDown()};
+    document.body.onkeydown = function() {onKeyDown()};
+    document.body.onkeyup = function() {onKeyUp()};
+}
+
+function onMouseMove() {
+    pList[0].innerHTML = `Navegador [${event.clientX}, ${event.clientY}]`;   
+    pList[1].innerHTML = `Pàgina [${event.screenX}, ${event.screenY}]`;  
+}
+
+function onMouseDown() {
+    this.changeColor('yellow');
+    if (event.which === 1) {
+        pList[2].innerHTML = "Clic esquerre Sí";
     }
-    document.getElementById('info').onmouseup = function() {
-        if (event.which === 1) {
-            pList[2].innerHTML = "Clic esquerre No";
-        }
-        if (event.which === 3) {
-            pList[3].innerHTML = "Clic dret No";
-        }
+    if (event.which === 2) {
+        pList[3].innerHTML = "Clic centre Sí";
     }
-    document.getElementById('info').onmousedown = function() {
-        if (event.which === 1) {
-            pList[2].innerHTML = "Clic esquerre Sí";
-        }
-        if (event.which === 3) {
-            pList[3].innerHTML = "Clic dret Sí";
-        }
+    if (event.which === 3) {
+        pList[4].innerHTML = "Clic dret Sí";
+    }
+}
+
+function onMouseUp() {
+    this.changeColor('white');
+    if (event.which === 1) {
+        pList[2].innerHTML = "Clic esquerre No";
+    }
+    if (event.which === 2) {
+        pList[3].innerHTML = "Clic centre No";
+    }
+    if (event.which === 3) {
+        pList[4].innerHTML = "Clic dret No";
+    }
+}
+
+function onKeyDown() {
+    this.changeColor('blue');
+    pList[5].innerHTML = `Caràcter [${event.key}]`;
+    pList[6].innerHTML = `Codi [${event.keyCode}]`;
+    this.hideMouseDisplay(true);
+}
+
+function onKeyUp() {
+    this.changeColor('white');
+    this.hideMouseDisplay(false);
+}
+
+function changeColor(color) {
+    document.getElementById('info').style['background-color'] = color;
+}
+
+function hideMouseDisplay(hideMouse) {
+    for (let p of pList) {
+        document.getElementsByTagName('h1')[0].innerHTML = hideMouse ? 'Teclat' : 'Ratolí';
+        p.style.display = hideMouse ? 'none' : 'block';
+        pList[5].style.display = hideMouse ? 'block' : 'none';
+        pList[6].style.display = hideMouse ? 'block' : 'none';
     }
 }
